65a290dcb17e1cc2bf72eb4d982e0780d055d9a1
Vladimir <mr.qrsant@gmail.com>
2019-09-11 18:55:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {Services} from './components/Services';
+import {Service} from './components/Service';
+import {Loading} from './components/Loading';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"; 
+import {useSelector} from'react-redux';
+function App() {
+  const state =  useSelector(state => state);
+  const isLoading = state.service.isLoading || state.services.isLoading;
+  return (
+    <Router>
+      { (isLoading) && <Loading isLoading />}
+      <Switch>
+        <Route path="/services/:id" exact component={Service} />
+        <Route path="/services" component={Services} />
+        <Route path="/" exact component={ () => <Redirect to={`/services`} /> } />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
